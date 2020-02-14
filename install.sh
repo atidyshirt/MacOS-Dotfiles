ecd0b3396e6a414441a33136cd587ac15f9e6fa6
Jordan <pyjordy@gmail.com>
2020-02-14 14:23:07 +1300

    Updating dots - after testing install

--- install.sh
+++ install.sh
@@ -1,13 +1,4 @@
 #bin/bash
-
-#NOTE: Huge thanks to all the authors of these lovely plugins, apps ect
-#      makes my life easy :)
-
-echo "We will need your password to change shell to zsh"
-chsh -s /bin/zsh
-
-#installing homebrew from Curl
-
 #install important packages and dependencies for config
 #BREW
 brew install pip3
@@ -19,33 +10,52 @@ brew install khanhas/tap/spicetify-cli
 brew install sassc
 brew tap homebrew/cask-fonts
 brew cask install font-firacode-nerd-font 
-
-gPIP
+brew install lazygit
+brew install lazydocker
 pip3 install pywal
 
 #CURL install OHMYYZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#installing default directories and dots
-mv Files ~/Documents/Files
-mv scripts ~/scripts
-mv .config ~/.config
-mv bin ~/bin
-mv spicetify_data ~/spicetify_data
+#installing dots
+mv Scripts $HOME/Scripts
+mv .config $HOME/.config
+mv bin $HOME/bin
+mv spicetify_data $HOME/spicetify_data
+mv .zshrc $HOME/.zshrc
+mv .profile $HOME/.profile
+mv wallpapers $HOME/Pictures
+mkdir $HOME/Documents/Files
+mkdir $HOME/Documents/Files/Bash
+mkdir $HOME/Documents/Files/Java
+mkdir $HOME/Documents/Files/Python
+mkdir $HOME/Documents/Files/Temp
+mkdir $HOME/Documents/Files/Univercity
+mkdir $HOME/Documents/Files/Temp
+mkdir $HOME/Documents/Files/Stenography
 
 #setting up OHMYZSH
 git clone https://github.com/reobin/typewritten.git $ZSH_CUSTOM/themes/typewritten
-ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+git clone https://github.com/GideonWolfe/vim.reaper.git
+cd vim.reaper
+sh update_config.sh
 
 #setting up ubersicht
 #Pecan
 git clone https://github.com/zzzeyez/pecan.git "$HOME/Library/Application\ Support/Übersicht/widgets/pecan"
-ln -s "${HOME}/Library/Application\ Support/Übersicht/widgets/pecan/pecan-style" "/usr/local/bin/pecan-style"
 pecan-style --load medicine
 
 #notifications
 https://github.com/zzzeyez/xanthia.git "$HOME/Library/Application\ Support/Übersicht/widgets/xanthia"
-ln -s "${HOME}/Library/Application\ Support/Übersicht/widgets/xanthia/notify-send" "/usr/local/bin/notify-send"
 
-source ~/.zshrc
-source ~/.profile
+#linking files after installing
+ln -s "${HOME}/Library/Application\ Support/Übersicht/widgets/xanthia/notify-send" "/usr/local/bin/notify-send"
+ln -s "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+ln -s "${HOME}/Library/Application\ Support/Übersicht/widgets/pecan/pecan-style" "/usr/local/bin/pecan-style"
+
+#telling the user
+echo "Restart Iterm2 to take effect"
+echo "open vim and type :PlugInstall"
+echo "check file paths are present"
+echo "check that spicetify applied"
+echo "          Enjoy :)          "
