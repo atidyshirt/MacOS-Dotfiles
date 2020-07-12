ca1c9e2e2d9da4122ce35e9e4b7309fde866d902
Jordan Pyott <jordanpyott@gmail.com>
2020-07-12 18:59:28 +1200

    Giving user feedback + installing spicetify for spotify

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # This is an installer of all the dependencies and my dotfiles
 # Prerequisites to all of the files
+echo "Installing ohmyzsh"
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Homebrew PKGs moved to brewdepen.sh
 echo "Installing Homebrew and it's custom packages" 
@@ -14,6 +15,7 @@ brew install ffsend
 brew install sassc
 brew tap homebrew/cask-fonts
 brew cask install font-firacode-nerd-font 
+brew install khanhas/tap/spicetify-cli
 brew install lazygit
 brew install lazydocker
 pip3 install pywal
@@ -22,10 +24,13 @@ brew cask install ubersicht
 brew install koekeishiya/formulae/skhd
 brew install koekeishiya/formulae/yabai 
 #Clone Dotfile Repo 
+echo "Cloning Dotfiles"
 git clone https://github.com/atidyshirt/MacOS-Dotfiles.git 
 #Clone bar into default Übersicht location
 git clone https://github.com/atidyshirt/simple-bar.git $HOME/Library/Application\ Support/Übersicht/widgets/simple-bar
  cd MacOS-Dotfiles
+
+ echo "Clearing old dotfiles"
 rm -rf $HOME/.config
 rm -rf $HOME/scripts
 rm -rf $HOME/spicetify_data/Themes
@@ -36,6 +41,7 @@ rm -rf $HOME/.Xresources
 rm -rf $HOME/bin
 rm -rf $HOME/.yabairc 
 
+echo "Installing Dotfiles from Cloned Repo"
 mv scripts $HOME/scripts
 mv .config $HOME/.config
 mv bin $HOME/bin
@@ -46,4 +52,7 @@ mv .Xresources $HOME/.Xresources
 mv wallpapers $HOME/Pictures
 mv .yabairc $HOME/.yabairc 
 
+echo "Starting pywalfox setup"
 sudo pywalfox setup
+
+echo "Done!"
